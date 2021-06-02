57bc61660b14074e0e72863bbded3d9e0c79107a
jhayes99823 <hayesja99@gmail.com>
2021-06-02 14:48:28 -0500

    reset the quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,12 @@ class Quiz extends Component {
         }
     }
 
+    handleResetClick() {
+        this.setState({
+            quiz_position: 1
+        })
+    }
+
     showNextQuestion() {
         this.setState({
             quiz_position: this.state.quiz_position + 1
@@ -22,7 +28,7 @@ class Quiz extends Component {
         const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length;
         return (
             <div className="QuizQuestion">
-                {isQuizEnd == true ? <QuizEnd /> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+                {isQuizEnd == true ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
             </div>
         )
     }
